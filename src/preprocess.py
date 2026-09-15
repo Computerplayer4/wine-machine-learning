73ae60f219dcbddbd5b81711065aca77b8636144
Computerplayer4 <hopettin@gmail.com>
2026-09-15 19:20:26 +0300

    add data processing and correlation figure

--- src/preprocess.py
+++ src/preprocess.py
@@ -27,3 +27,28 @@ y = df_wine['quality'].to_numpy()
 # Splitting the dataset into training (60 %), validation (20 %) and test (20 %) sets
 X_train, X_val_test, y_train, y_val_test = train_test_split(X, y, test_size=0.4, random_state=42)
 X_val, X_test, y_val, y_test = train_test_split(X_val_test, y_val_test, test_size=0.5, random_state=42)
+
+# Normalize the data to zero mean and unit variance
+# We only use training data when calculating mu and sigma as we want to prevent data leakage
+# These values will be used with training and validation data only
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_val_scaled = scaler.transform(X_val)
+X_test_scaled = scaler.transform(X_test)
+
+# Generate EDA statistics
+print("--- DATASET SUMMARY ---")
+print(f"Total samples (N): {len(df_wine)}")
+print(f"Feature vector dimension (d): {X.shape[1]}")
+print(f"Train size: {len(X_train)}, Val size: {len(X_val)}, Test size: {len(X_test)}\n")
+
+print(f"--- FEATURE SUMMARY STATISTICS ---")
+print(df_wine.describe().T[['mean', 'std', 'min', 'max']])
+
+# Generate Pearson Correlation Matrix Heatmap
+plt.figure(figsize=(10, 8))
+sns.heatmap(df_wine.corr(), annot=True, fmt=".2f", cmap="coolwarm")
+plt.title("Pearson Correlation Matrix - Combined Wine Quality")
+plt.tight_layout()
+plt.savefig("../assets/correlation_matrix.png", dpi=300)
+plt.show()
