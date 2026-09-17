3f622e73ca8e0499dd34173dd195231e5f08a5bf
Computerplayer4 <hopettin@gmail.com>
2026-09-17 19:39:20 +0300

    add more figures to be generated

--- src/preprocess.py
+++ src/preprocess.py
@@ -47,6 +47,30 @@ print(f"Train size: {len(X_train)}, Val size: {len(X_val)}, Test size: {len(X_te
 print(f"--- FEATURE SUMMARY STATISTICS ---")
 print(df_wine.describe().T[['mean', 'std', 'min', 'max']])
 
+# Generate wine counts figure
+
+wine_counts = df_wine['is white'].value_counts()
+plt.figure(figsize=(6, 4))
+# Here 0 is Red an 1 is White as before
+plt.bar(['Red Wine', 'White Wine'], [wine_counts.get(0, 0), wine_counts.get(1, 0)], color=['#800020', '#F0E68C'], edgecolor='black')
+plt.title('Dataset split: Red vs. White Wine Samples')
+plt.xlabel('Wine Type')
+plt.ylabel('Number of Samples (Count)')
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.savefig('../assets/wine_type_split.png')
+plt.show()
+
+# Generate score histogram
+plt.figure(figsize=(8, 5))
+plt.hist(df_wine['quality'], bins=range(3,11), align='left', rwidth=0.85, color='purple', edgecolor='black', alpha=0.8)
+plt.title('Distribution of Wine Quality Scores')
+plt.xlabel('Quality Score (Rating)')
+plt.ylabel('Frequency (Number of Samples)')
+plt.xticks(range(3, 10))
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.savefig('../assets/quality_histogram.png')
+plt.show()
+
 # Generate Pearson Correlation Matrix Heatmap
 plt.figure(figsize=(10, 8))
 sns.heatmap(df_wine.corr(), annot=True, fmt=".2f", cmap="coolwarm")
